58e25d08afb43fbd0fb1f442cddc0c2270cfe287
Ibrahima DIALLO <ibrahima.diallo@student.yncrea.fr>
2020-11-25 16:22:06 +0100

    start

--- EXO1/boucle.c
+++ EXO1/boucle.c
@@ -3,24 +3,25 @@
 #include <math.h>
 
 int main() {
-	int n = 511, i;
-	unsigned short int somme = 0;
-	/*for (i = 1; i <= n; i++) {
-		somme += i;
-	}*/
-	/*i = 1;
-	while (i <= n) {
-		somme += i;
-		i++;
-	}*/
+	int n = 100, i;
+	unsigned short int somme1 = 0, somme2 = 0, somme3 = 0;
+	for (i = 1; i <= n; i++) {
+		somme1 += i;
+	}
+	printf("1er somme : %d\n", somme1);
 	i = 1;
-	/*do {
-		somme += i;
-		i++;
-	} while (i <= n);*/
 
-	//printf("%d", somme);
-	int k;
-	k = pow(2, 16);
-	printf("%d",k);
+	while (i <= n) {
+		somme2 += i;
+		i++;
+	}
+	printf("2e somme : %d\n", somme2);
+	i = 1;
+	do {
+		somme3 += i;
+		i++;
+	} while (i <= n);
+
+	printf("3e somme : %d\n", somme3);
+	
 }
--- EXO2/main.c
+++ EXO2/main.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-	float d1, d2, d3;
-	int b;
-	printf("Entrez le dimensions : ");
-	scanf_s("%f %f %f", &d1, &d2, &d3);
-	do {
+	float d1, d2, d3; 
+	int reset = 0;    
+	
+	while (reset == 0) {
+		printf("Entrez le dimensions : ");
+		scanf_s("%f %f %f", &d1, &d2, &d3);
+		
 		if ((d1 >= 150 || d1 <= 1) || (d2 >= 150 || d2 <= 1) || (d3 >= 150 || d3 <= 1)) {
-			printf("NON VALIDE");
+			printf("NON VALIDE\n");
 		}
+		
 		else {
 			printf("VALIDE\n");
 		}
-		printf("Voulez vous enregistrez de nouveau ? ");
-		scanf_s("%d", &b);
+
+		printf("Voulez vous enregistrer ? Si oui Tapez sur la touche '0' : ");
+		scanf_s("%d", &reset);
+
+	}
+
 	
-	} while (b != 0);
 }
+
+  
