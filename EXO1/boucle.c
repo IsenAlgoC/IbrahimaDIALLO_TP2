f88e02a2ad7d746fd7e75b110578ecda88a657c7
Ibrahima DIALLO <ibrahima.diallo@student.yncrea.fr>
2020-11-26 11:15:56 +0100

    start

--- EXO1/boucle.c
+++ EXO1/boucle.c
@@ -5,23 +5,30 @@
 int main() {
 	int n = 100, i;
 	unsigned short int somme1 = 0, somme2 = 0, somme3 = 0;
+	/*========== Somme des n premiers entiers positifs avec  boucles différentes ==========*/
+	
+	// 1ere Boucle 
 	for (i = 1; i <= n; i++) {
 		somme1 += i;
 	}
-	printf("1er somme : %d\n", somme1);
-	i = 1;
+	
+	// 2e Boucle
 
+	i = 1;
 	while (i <= n) {
 		somme2 += i;
 		i++;
 	}
-	printf("2e somme : %d\n", somme2);
+	
+	// 3e Boucle 
+
 	i = 1;
 	do {
 		somme3 += i;
 		i++;
 	} while (i <= n);
 
-	printf("3e somme : %d\n", somme3);
+	/*========== Trouver la valeur maximum de n sans dépassement de capacité ==========*/
+
 	
 }
